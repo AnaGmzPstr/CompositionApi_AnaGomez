1f2043a330f9493ea85862a387a397e8c4af3e6f
Ana Gomez <ana.gomez.7e6@itb.cat>
2024-11-07 17:42:55 +0100

    First commit

---
+++ vue-project/src/App.vue
@@ -0,0 +1,21 @@
+<!-- App vue -->
+<script setup>
+import Lista from './components/ListaProd.vue'
+import { ref } from 'vue'
+const nomComanda = ref("");
+</script>
+
+<template>
+  <div class="container">
+    <h1>{{ nomComanda }} </h1>
+    <input type="text" placeholder="Nom comanda" v-model="nomComanda">
+    <Lista/>
+  </div>
+</template>
+
+<style scoped>
+.container{
+  display: grid;
+  justify-content: center;
+}
+</style>
---
+++ vue-project/src/components/ListaProd.vue
@@ -0,0 +1,29 @@
+<template>
+    <h3>Productes disponibles</h3>
+    <ul 
+    v-for="(product, index) in productos"
+    :key="index"
+    >
+    <li>
+        <p>{{ product.name }} - Preu: {{ product.price }}</p>
+        <button @click="afegir">Afegir</button>
+    </li>
+
+    </ul>
+
+</template>
+
+<script setup>
+const productos = [
+    { name: "Hamburger 🍔", price: 5 },
+    { name: "Cheeseburger 🧀", price: 6 },
+    { name: "Impossible Burger (vegana)🤮", price: 7 },
+    { name: "Fries 🍟", price: 2 }
+]
+</script>
+
+<style>
+li{
+    list-style: none;
+}
+</style>
